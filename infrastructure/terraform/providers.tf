8911fed1a769c7a37ead229bbd819e8ef05c9a44
chukwukelu2023 <chukwukeluoo@gmail.com>
2026-09-20 18:14:51 +0100

    fix: edit spn vars in provider block

--- infrastructure/terraform/providers.tf
+++ infrastructure/terraform/providers.tf
@@ -45,7 +45,7 @@ provider "cloudflare" {
   #   api_token = var.cloudflare_api_token
 }
 
-provider "azapi" {
-  use_cli = false
-  use_msi = false
-}
+# provider "azapi" {
+#   use_cli = false
+#   use_msi = false
+# }
